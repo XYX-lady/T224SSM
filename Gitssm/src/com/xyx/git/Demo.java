009abb49f36eb0faaa7fc98311b553291fecc254
XYX-lady <Xie_lady@163.com>
2019-10-17 23:39:14 +0800

    第一次提交

---
+++ Gitssm/src/com/xyx/git/Demo.java
@@ -0,0 +1,13 @@
+package com.xyx.git;
+
+/**
+ * @author xyx
+ * @site www.xyxmage.com
+ * @company xxx公司
+ * @create 2019-10-17 22:45
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈");
+    }
+}

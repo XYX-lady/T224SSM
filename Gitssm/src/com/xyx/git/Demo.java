51738dae7ea8e14290ae56f2be624c509647c966
XYX-lady <Xie_lady@163.com>
2019-10-18 09:52:40 +0800

    第二次提交

--- Gitssm/src/com/xyx/git/Demo.java
+++ Gitssm/src/com/xyx/git/Demo.java
@@ -8,6 +8,6 @@ package com.xyx.git;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈  update");
     }
 }
